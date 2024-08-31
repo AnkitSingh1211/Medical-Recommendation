00a591046421047d41cebcb9f801f23747a4ce24
Ankit-singh <152522109+AnkitSingh1211@users.noreply.github.com>
2024-08-31 21:49:28 +0530

    Second commit

---
+++ README.md
@@ -0,0 +1,2 @@
+It is a Machine learning Project.
+Model will Predict disease base on symptom and also do medical recommendation.
---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import sklearn
+
+st.title('Disease Prediction and Medical Recommendation')
